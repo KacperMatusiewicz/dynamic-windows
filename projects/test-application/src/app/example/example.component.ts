3bdf9cb3ead23e431b5757cde1d66557f5fa02a6
OwczarekGit <32412218+OwczarekGit@users.noreply.github.com>
2022-11-07 19:08:32 +0100

    Set windowName for Example

--- projects/test-application/src/app/example/example.component.ts
+++ projects/test-application/src/app/example/example.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {DynamicWindow} from "dynamic-windows-core";
 
 
@@ -9,8 +9,12 @@ import {DynamicWindow} from "dynamic-windows-core";
 })
 export class ExampleComponent extends DynamicWindow implements OnInit {
 
+  @ViewChild("inputField")
+  inputField!: ElementRef<HTMLTextAreaElement>
+
   constructor() {
     super();
+    this.windowName = "Text Editor"
   }
 
   ngOnInit(): void {
@@ -24,6 +28,11 @@ export class ExampleComponent extends DynamicWindow implements OnInit {
       }
 
     }
+  }
+
+  public updateTitle($event: KeyboardEvent){
+    console.log($event)
+    this.windowName = this.inputField.nativeElement.value
   }
 
 }
